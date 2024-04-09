3480fd3a7f7386e47a6fc13bb1d80fcadc080513
김진욱 <165977516+jin-dol-code-1227@users.noreply.github.com>
2024-04-09 19:51:34 +0900

    [Rename] 버튼 컴포넌트 개발
    core-ui/#2
    
    - type, util 관련 파일 연관된 컴포넌트를 공통적으로 관리하도록 수정

--- src/type/components/atoms/buttons/Button.ts
+++
@@ -1,7 +0,0 @@
-import { ButtonHTMLAttributes } from 'react';
-
-export type ButtonVariants = 'primary' | 'secondary';
-
-export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  buttonVariant: ButtonVariants;
-}
